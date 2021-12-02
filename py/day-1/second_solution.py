a8e6d7a277eda565f84edeecd8cb4dc8cea4c80c
ilker <ilkerkaran@gmail.com>
2021-12-02 16:34:26 +0000

    day-1 second-answer py completed

--- py/day-1/second_solution.py
+++ py/day-1/second_solution.py
@@ -1,2 +1,12 @@
-def secondSolution(arr):
-  print("this is secondSolution")
+def secondSolution(inputArr):
+
+  def addition(i):
+    return int(i)
+  arr = map(addition, inputArr)
+  c=0
+  for i, n in enumerate(arr):
+       if (i>0 and i< (len(arr)-2)):
+        #  print(i)
+         if(arr[i + 2] > arr[i - 1]):
+            c += 1 
+  return c
