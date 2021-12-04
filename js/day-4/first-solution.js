90f2c00be59ca5418d337ed60c39e1bd87fc28aa
ilker <ilkerkaran@gmail.com>
2021-12-04 20:15:57 +0000

    complete js day-4

---
+++ js/day-4/first-solution.js
@@ -0,0 +1,70 @@
+module.exports = (numbers, cards) => {
+  let res = 0
+  const numArr = numbers.split(',').map((i) => +i)
+  let curState = {}
+  const cardMap = cards.reduce((acc, c, i) => {
+    const f = c.split('\n').map((r) => r.split(' ').filter((n) => n).map((n) => +n))
+    let m = {}
+    for (let j = 0; j < f.length; j++) {
+      const element = f[j]
+      for (let k = 0; k < element.length; k++) {
+        const elem = element[k]
+        m = { ...m, [elem]: [j, k] }
+      }
+    }
+
+    return {
+      ...acc,
+      [i]: { ...m }
+    }
+  }, {})
+
+  const progress = (curNum) => {
+    let bingo = -1
+    const array = Object.entries(cardMap)
+    for (let i = 0; i < array.length; i++) {
+      const card = array[i]
+      const cardIndex = card[0]
+      const cardNumbers = card[1]
+      if (cardNumbers[curNum]) {
+        curState = {
+          ...curState,
+          [cardIndex]: {
+            r: {
+              ...(curState[i] || {}).r,
+              [cardNumbers[curNum][0]]: (+(curState[cardIndex] ? curState[cardIndex].r[cardNumbers[curNum][0]] || 0 : 0) + 1)
+            },
+            c: {
+              ...(curState[i] || {}).c,
+              [cardNumbers[curNum][1]]: (+(curState[cardIndex] ? curState[cardIndex].c[cardNumbers[curNum][1]] || 0 : 0) + 1)
+            }
+          }
+        }
+        cardNumbers[curNum] = undefined
+        if (Object.values(curState[cardIndex].r).some((x) => +x > 4) || Object.values(curState[cardIndex].c).some((x) => +x > 4)) {
+          bingo = i
+          console.log('bingooo')
+          i = array.length
+        }
+      }
+    }
+    return bingo
+  }
+
+  const finalise = (cardIndex, curNum) => {
+    const card = cardMap[cardIndex]
+    const sum = Object.keys(card).filter((k) => !!card[k]).reduce((acc, n) => +n + acc, 0)
+    return sum * +curNum
+  }
+
+  for (let i = 0; i < numArr.length; i++) {
+    const curNum = numArr[i]
+    const h = progress(curNum)
+    if (h > -1) {
+      res = finalise(h, curNum)
+      break
+    }
+  }
+
+  return res
+}
