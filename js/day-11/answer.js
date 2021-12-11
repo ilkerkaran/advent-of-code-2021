cabe16b7a649b35394e82e824b1fd47854d5667e
ilker <ilkerkaran@gmail.com>
2021-12-11 18:55:10 +0000

    complete js day-11

---
+++ js/day-11/answer.js
@@ -0,0 +1,12 @@
+const input = require('./input')
+const dummyInput = require('./dummyInput')
+const firstSolution = require('./first-solution')
+const secondSolution = require('./second-solution')
+
+const initialState = input.split('\n').map((i) => i.split('').map((j) => +j))
+
+const dummyInitialState = dummyInput.split('\n').map((i) => i.split('').map((j) => +j))
+console.log(`First Dummy Answer is ${firstSolution(dummyInitialState, 100)}`)
+console.log(`Second Dummy Answer is ${secondSolution(dummyInitialState)}`)
+console.log(`First Answer is ${firstSolution(initialState, 100)}`)
+console.log(`Second Answer is ${secondSolution(initialState)}`)
---
+++ js/day-11/first-solution.js
@@ -0,0 +1,85 @@
+module.exports = (inputMap, step) => {
+  let res = 0
+
+  const increasePos = (i, j) => {
+    if (inputMap[i] && (inputMap[i][j] != undefined)) {
+      inputMap[i][j] += 1
+      return inputMap[i][j]
+    }
+    return null
+  }
+
+  const increaseAdjesants = (i, j, m) => {
+    if (!m[`${i}_${j}`]) {
+      m[`${i}_${j}`] = true
+      const n = increasePos(i, j - 1)
+      const ne = increasePos(i - 1, j + 1)
+      const e = increasePos(i, j + 1)
+      const se = increasePos(i + 1, j + 1)
+      const s = increasePos(i + 1, j)
+      const sw = increasePos(i + 1, j - 1)
+      const w = increasePos(i - 1, j)
+      const nw = increasePos(i - 1, j - 1)
+
+      checkAndIncrement(n, i, j - 1, m)
+      checkAndIncrement(ne, i - 1, j + 1, m)
+      checkAndIncrement(e, i, j + 1, m)
+      checkAndIncrement(se, i + 1, j + 1, m)
+      checkAndIncrement(s, i + 1, j, m)
+      checkAndIncrement(sw, i + 1, j - 1, m)
+      checkAndIncrement(w, i - 1, j, m)
+      checkAndIncrement(nw, i - 1, j - 1, m)
+    }
+  }
+
+  const checkAndIncrement = (num, i, j, m) => {
+    if (num > 9 && inputMap[i] && (inputMap[i][j] != undefined)) {
+      increaseAdjesants(i, j, m)
+    }
+  }
+  const proceed = (arr) => {
+    let flashCount = 0
+    // normal increase
+    for (let i = 0; i < arr.length; i++) {
+      const line = arr[i]
+      for (let j = 0; j < line.length; j++) {
+        const c = increasePos(i, j)
+      }
+    }
+
+    // increase because of flashes
+    const triggerMap = {}
+    for (let i = 0; i < arr.length; i++) {
+      const line = arr[i]
+      for (let j = 0; j < line.length; j++) {
+        if (inputMap[i][j] > 9) {
+          increaseAdjesants(i, j, triggerMap)
+        }
+      }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr[i].length; j++) {
+        const e = inputMap[i][j]
+        if (e > 9) {
+          inputMap[i][j] = 0
+          flashCount++
+        }
+      }
+    }
+
+    return flashCount
+  }
+  const printArr = (desc) => {
+    const p = inputMap.map((i) => i.join(',')).join('\n')
+    console.log('arr', desc, `\n${p}`)
+  }
+
+  for (let i = 0; i < step; i++) {
+    const curFlashes = proceed(inputMap)
+    if (i <= 10 || (i + 1) % 10 == 0) { printArr(`${curFlashes}, iteration ${i + 1}`) }
+    res += curFlashes
+  }
+
+  return res
+}
